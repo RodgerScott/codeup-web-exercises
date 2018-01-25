22e4cedebd7d627b390f76937b2b403ae2d3ce8f
RodgerScott <rodger.scott@gmail.com>
2018-01-25 16:37:12 -0600

    external && internal js practice files.

---
+++ js/external.js
@@ -0,0 +1,10 @@
+alert("Hey Dingus, Welcome To My Website!");
+
+// var confirmed = confirm("IS your hand bigger than your face?");
+//
+// console.log(confirmed);
+
+
+var userInput = prompt('What is your favorite color?');
+
+console.log("What a coincidence, " + userInput + " is my favorite color too!");
