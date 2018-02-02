b60613b948b0604cad6be063467e8f12295a0a7d
RodgerScott <rodger.scott@gmail.com>
2018-02-02 11:57:24 -0600

    Just messing around.

---
+++ js/messing-around.js
@@ -0,0 +1,18 @@
+(function () {
+
+var response = prompt("Add student" + '\n' + "Remove Student" + '\n' + "List Students" + '\n' + "Quit");
+
+function studentRoster() {
+
+    var array = [];
+
+        do {
+
+
+        } while (response !== 'quit');
+
+    }
+})();
+
+
+//Check out Wo Fat
