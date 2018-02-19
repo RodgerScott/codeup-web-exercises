a9c4fb4439d1b8dba84eafe0dc7624bb136b7fed
RodgerScott <rodger.scott@gmail.com>
2018-02-19 11:58:32 -0600

    refactoring using regex

--- js/pig-latin.js
+++ js/pig-latin.js
@@ -7,24 +7,21 @@ function  ted () {
                 while (newPhrase[i] === "") {
                     newPhrase.shift(i);
                 }
-                if (newPhrase[i].toLowerCase().charCodeAt(0) === 97 || newPhrase[i].toLowerCase().charCodeAt(0) === 101 ||
-                    newPhrase[i].toLowerCase().charCodeAt(0) === 105 || newPhrase[i].toLowerCase().charCodeAt(0) === 111 ||
-                    newPhrase[i].toLowerCase().charCodeAt(0) === 117) {
+                if (newPhrase[i].charAt(0).match(/[aeiou]/gi)) {
+
                     pigLatingCharacter.push(newPhrase[i] + "way");
                 }
-                else if (newPhrase[i].toLowerCase().charCodeAt(1) === 97 || newPhrase[i].toLowerCase().charCodeAt(1) === 101 ||
-                        newPhrase[i].toLowerCase().charCodeAt(1) === 105 || newPhrase[i].toLowerCase().charCodeAt(1) === 111 ||
-                        newPhrase[i].toLowerCase().charCodeAt(1) === 117 || newPhrase[i].toLowerCase().charCodeAt(1) === 121)  {
+                else if (newPhrase[i].charAt(1).match(/[aeiou]/gi)) {
+
                     pigLatingCharacter.push(newPhrase[i].substring(1, newPhrase[i].length) + (newPhrase[i].charAt(0).toLowerCase() + "ay"));
+
                 }
-                else if (newPhrase[i].toLowerCase().charCodeAt(2) === 97 || newPhrase[i].toLowerCase().charCodeAt(2) === 101 ||
-                        newPhrase[i].toLowerCase().charCodeAt(2) === 105 || newPhrase[i].toLowerCase().charCodeAt(2) === 111 ||
-                        newPhrase[i].toLowerCase().charCodeAt(2) === 117 || newPhrase[i].toLowerCase().charCodeAt(2) === 121) {
+                else if (newPhrase[i].charAt(2).match(/[aeiou]/gi)) {
+
                     pigLatingCharacter.push(newPhrase[i].substring(2, newPhrase[i].length) + (newPhrase[i].substring(0, 2).toLowerCase() + "ay"));
                 }
-                else if (newPhrase[i].toLowerCase().charCodeAt(3) === 97 || newPhrase[i].toLowerCase().charCodeAt(3) === 101 ||
-                    newPhrase[i].toLowerCase().charCodeAt(3) === 105 || newPhrase[i].toLowerCase().charCodeAt(3) === 111 ||
-                    newPhrase[i].toLowerCase().charCodeAt(3) === 117 || newPhrase[i].toLowerCase().charCodeAt(3) === 121) {
+                else if (newPhrase[i].charAt(3).match(/[aeiou]/gi)) {
+
                     pigLatingCharacter.push(newPhrase[i].substring(3, newPhrase[i].length) + (newPhrase[i].substring(0, 3).toLowerCase() + "ay"));
                 }
                 else {
