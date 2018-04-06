70f4145029f8eab73e1254d2b0a6102659c3c865
RodgerScott <rodger.scott@gmail.com>
2018-04-06 14:18:13 -0500

    added functionality to keep players from marking board before game starts or after game finishes

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -4,132 +4,137 @@ var ticTacToeBoard = [[0,0,0],[0,0,0],[0,0,0]];
 
 var turn = 0;
 
-$('.box').click(function(event){
-
-    if (turn % 2 === 0 && $(this).text() === "") {
-        $(event.target).text('X');
-        turn += 1;
-        $('#turnIndicator').text("Go Player O");
-    } else if (turn % 2 === 1 && $(this).text() === "") {
-        $(event.target).text("O");
-        turn += 1;
-        $('#turnIndicator').text("Go Player X");
-    }
-});
+var active = false;
 
 $('button').click(function(event) {
     event.preventDefault();
+    active = true;
     $('.box').text("");
     $('#turnIndicator').text("Player X's Turn");
     turn = 0;
     ticTacToeBoard = [[0,0,0],[0,0,0],[0,0,0]];
 });
 
+    $('.box').click(function (event) {
 
-$('#tr0').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[0][0] === 0) {
+        if (turn % 2 === 0 && $(this).text() === "" && active === true) {
+            $(event.target).text('X');
+            turn += 1;
+            $('#turnIndicator').text("Go Player O");
+        } else if (turn % 2 === 1 && $(this).text() === "" && active === true) {
+            $(event.target).text("O");
+            turn += 1;
+            $('#turnIndicator').text("Go Player X");
+        }
+    });
 
-        ticTacToeBoard[0].splice(0, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[0][0] === 0) {
-        ticTacToeBoard[0].splice(0, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
 
-$('#tr1').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[0][1] === 0) {
+    $('#tr0').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[0][0] === 0) {
 
-        ticTacToeBoard[0].splice(1, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[0][1] === 0){
-        ticTacToeBoard[0].splice(1, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
-$('#tr2').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[0][2] === 0) {
+            ticTacToeBoard[0].splice(0, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[0][0] === 0) {
+            ticTacToeBoard[0].splice(0, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
 
-        ticTacToeBoard[0].splice(2, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[0][2] === 0) {
-        ticTacToeBoard[0].splice(2, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
+    $('#tr1').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[0][1] === 0) {
 
-$('#mr0').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[1][0] === 0) {
+            ticTacToeBoard[0].splice(1, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[0][1] === 0) {
+            ticTacToeBoard[0].splice(1, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
+    $('#tr2').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[0][2] === 0) {
 
-        ticTacToeBoard[1].splice(0, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[1][0] === 0) {
-        ticTacToeBoard[1].splice(0, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
+            ticTacToeBoard[0].splice(2, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[0][2] === 0) {
+            ticTacToeBoard[0].splice(2, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
 
-$('#mr1').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[1][1] === 0) {
+    $('#mr0').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[1][0] === 0) {
 
-        ticTacToeBoard[1].splice(1, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[1][1] === 0) {
-        ticTacToeBoard[1].splice(1, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
-$('#mr2').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[1][2] === 0) {
+            ticTacToeBoard[1].splice(0, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[1][0] === 0) {
+            ticTacToeBoard[1].splice(0, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
 
-        ticTacToeBoard[1].splice(2, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[1][2] === 0) {
-        ticTacToeBoard[1].splice(2, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
+    $('#mr1').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[1][1] === 0) {
 
-$('#br0').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[2][0] === 0) {
+            ticTacToeBoard[1].splice(1, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[1][1] === 0) {
+            ticTacToeBoard[1].splice(1, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
 
-        ticTacToeBoard[2].splice(0, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[2][0] === 0) {
-        ticTacToeBoard[2].splice(0, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
+    $('#mr2').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[1][2] === 0) {
 
-$('#br1').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[2][1] === 0) {
+            ticTacToeBoard[1].splice(2, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[1][2] === 0) {
+            ticTacToeBoard[1].splice(2, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
 
-        ticTacToeBoard[2].splice(1, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[2][1] === 0){
-        ticTacToeBoard[2].splice(1, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
-$('#br2').click(function(){
-    if(turn % 2 === 0 && ticTacToeBoard[2][2] === 0) {
+    $('#br0').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[2][0] === 0) {
 
-        ticTacToeBoard[2].splice(2, 1, 1);
-        console.log(ticTacToeBoard);
-    } else if (turn % 2 === 1 && ticTacToeBoard[2][2] === 0) {
-        ticTacToeBoard[2].splice(2, 1, 2);
-        console.log(ticTacToeBoard);
-    }
-    isSolved();
-});
+            ticTacToeBoard[2].splice(0, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[2][0] === 0) {
+            ticTacToeBoard[2].splice(0, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
+
+    $('#br1').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[2][1] === 0) {
+
+            ticTacToeBoard[2].splice(1, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[2][1] === 0) {
+            ticTacToeBoard[2].splice(1, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
+
+    $('#br2').click(function () {
+        if (turn % 2 === 0 && ticTacToeBoard[2][2] === 0) {
+
+            ticTacToeBoard[2].splice(2, 1, 1);
+            console.log(ticTacToeBoard);
+        } else if (turn % 2 === 1 && ticTacToeBoard[2][2] === 0) {
+            ticTacToeBoard[2].splice(2, 1, 2);
+            console.log(ticTacToeBoard);
+        }
+        isSolved();
+    });
 
 
 function isSolved() {
@@ -138,52 +143,68 @@ function isSolved() {
 
     if (ticTacToeBoard[0][0] === 1 && ticTacToeBoard[0][1] === 1 && ticTacToeBoard[0][2] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[1][0] === 1 && ticTacToeBoard[1][1] === 1 && ticTacToeBoard[1][2] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[2][0] === 1 && ticTacToeBoard[2][1] === 1 && ticTacToeBoard[2][2] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][0] === 2 && ticTacToeBoard[0][1] === 2 && ticTacToeBoard[0][2] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
     if (ticTacToeBoard[1][0] === 2 && ticTacToeBoard[1][1] === 2 && ticTacToeBoard[1][2] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
     if (ticTacToeBoard[2][0] === 2 && ticTacToeBoard[2][1] === 2 && ticTacToeBoard[2][2] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
 //vertical wins
     if (ticTacToeBoard[0][0] === 1 && ticTacToeBoard[1][0] === 1 && ticTacToeBoard[2][0] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][1] === 1 && ticTacToeBoard[1][1] === 1 && ticTacToeBoard[2][1] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][2] === 1 && ticTacToeBoard[1][2] === 1 && ticTacToeBoard[2][2] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][0] === 2 && ticTacToeBoard[1][0] === 2 && ticTacToeBoard[2][0] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][1] === 2 && ticTacToeBoard[1][1] === 2 && ticTacToeBoard[2][1] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][2] === 2 && ticTacToeBoard[1][2] === 2 && ticTacToeBoard[2][2] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
 //diagonalwins
     if (ticTacToeBoard[0][0] === 1 && ticTacToeBoard[1][1] === 1 && ticTacToeBoard[2][2] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][2] === 1 && ticTacToeBoard[1][1] === 1 && ticTacToeBoard[2][0] === 1) {
         $('#turnIndicator').text('O Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][0] === 2 && ticTacToeBoard[1][1] === 2 && ticTacToeBoard[2][2] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
     if (ticTacToeBoard[0][2] === 2 && ticTacToeBoard[1][1] === 2 && ticTacToeBoard[2][0] === 2) {
         $('#turnIndicator').text('X Wins');
+        active = false;
     }
 }
